9eae08c09ae55ed6c7a87765882a430d350eae10
Jeff-Adler <Jeffm.adler@gmail.com>
2020-10-07 07:44:37 -0400

    added logout and accountstacknavigator components

--- src/screens/Logout.js
+++ src/screens/Logout.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Button, StyleSheet } from "react-native";
 
-const Account = (props) => {
+const Logout = (props) => {
   const { logoutHandler } = props;
 
   return (
@@ -20,4 +20,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Account;
+export default Logout;
